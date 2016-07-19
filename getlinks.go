91c1a85079a082117c1f384fb2bc584699ce9df6
Sudarsan <sudar.theone@gmail.com>
2016-07-19 19:21:06 +0530

    first commit

---
+++ getlinks.go
@@ -0,0 +1,65 @@
+package getlinks
+
+import (
+	"io"
+	"strconv"
+	"strings"
+	"golang.org/x/net/html"
+)
+
+
+func All(httpBody io.Reader) []string {
+	links := []string{}
+	col := []string{}
+	page := html.NewTokenizer(httpBody)
+	for {
+		tokenType := page.Next()
+		if tokenType == html.ErrorToken {
+			return links
+		}
+		token := page.Token()
+		if tokenType == html.StartTagToken && token.DataAtom.String() == "a" {
+			for _, attr := range token.Attr {
+				if attr.Key == "href" {
+					tl := trimHash(attr.Val)
+					col = append(col, tl)
+					resolv(&links, col)
+				}
+			}
+		}
+	}
+}
+
+func trimHash(l string) string {
+	if strings.Contains(l, "#") {
+		var index int
+		for n, str := range l {
+			if strconv.QuoteRune(str) == "'#'" {
+				index = n
+				break
+			}
+		}
+		return l[:index]
+	}
+	return l
+}
+
+func check(sl []string, s string) bool {
+	var check bool
+	for _, str := range sl {
+		if str == s {
+			check = true
+			break
+		}
+	}
+	return check
+}
+
+
+func resolv(sl *[]string, ml []string) {
+	for _, str := range ml {
+		if check(*sl, str) == false {
+			*sl = append(*sl, str)
+		}
+	}
+}
